c3458eef5e53c9d4a17d0faf246a0570648ae985
Jacky Dai <ykcaj@g.ucla.edu>
2022-11-03 13:03:48 -0700

    added title

--- FrontEnd/hello-world-example/src/App.js
+++ FrontEnd/hello-world-example/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import './App.css';
 function App() {
    return (
       <div className="App">
-         Hello World !
+         <h1>DontBeFake.</h1>
       </div>
    );
 }
 export default App;
+
+
+
+
+
+
