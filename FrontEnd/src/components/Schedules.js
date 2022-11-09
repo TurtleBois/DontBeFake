e133c6e1fdada8c2a4845568e2ec02aebe1c75c9
Jacky Dai <ykcaj@g.ucla.edu>
2022-11-08 18:36:58 -0800

    added schedules and cards

---
+++ FrontEnd/src/components/Schedules.js
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+
+// export default class About extends Component {
+//     render() {
+//         return (
+//             <div>
+//                 <h2> TODO add box that can hover and cloick to go to indual schedules</h2>
+//             </div>
+//         )
+//     }
+// }
+
+
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+const bull = (
+  <Box
+    component="span"
+    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+  >
+    •
+  </Box>
+);
+
+
+function BasicCard() {
+    return (
+      <Card sx={{ minWidth: 275 }}>
+        <CardContent>
+          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+            Word of the Day
+          </Typography>
+          <Typography variant="h5" component="div">
+            be{bull}nev{bull}o{bull}lent
+          </Typography>
+          <Typography sx={{ mb: 1.5 }} color="text.secondary">
+            adjective
+          </Typography>
+          <Typography variant="body2">
+            well meaning and kindly.
+            <br />
+            {'"a benevolent smile"'}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
+    );
+  }
+
+export default function Schedule(){
+  return (
+    <div>
+        <BasicCard/>
+        <BasicCard/>
+        <BasicCard/>
+    </div>
+    
+  );
+}
