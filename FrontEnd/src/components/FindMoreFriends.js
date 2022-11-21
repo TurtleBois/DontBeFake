8d282824e26c915c898106356e0a72e903668a4f
nikki-ha <nikkiha811@gmail.com>
2022-11-21 14:39:42 -0800

    Finalized MyFriends page.

--- FrontEnd/src/components/FindMoreFriends.js
+++ FrontEnd/src/components/FindMoreFriends.js
@@ -4,7 +4,8 @@ import "../styles/Friend.css"
 const FindMoreFriends = () => {
     return(
         <div class="find-more-friends">
-            <img src={require('../assets/plus-sign.png')}/>
+            {/* Make image link to search page */}
+            <img alt="fmf-img" src={require('../assets/plus-sign.png')}/>
             <b><p id="find-more-friends-text">Find more friends</p></b>
         </div>
     )
--- FrontEnd/src/components/Friends.js
+++ FrontEnd/src/components/Friends.js
@@ -3,14 +3,14 @@ import "../styles/Friend.css"
 
 const Friend = () => {
     return(
-        <div class="friend">
+        <body class="friend">
             <img alt="pfp" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"/>
             <div class="friend-text">
-                <b><p id="friend-name"></p>PowellCat</b>
+                <b><p id="friend-name"></p>Powell Cat</b>
                 <b><p id="friend-username">campuscat</p></b>
                 <b><p id="friend-status">"🐈‍⬛"</p></b>
             </div>
-        </div>
+        </body>
     )
 }
 
