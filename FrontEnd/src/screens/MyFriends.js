3c1d62b2a5cea13a0a1461498175be19a0c35c41
nikki-ha <nikkiha811@gmail.com>
2022-11-21 20:29:42 -0800

    Stylistic changes to code for MyFriends.js

--- FrontEnd/src/screens/MyFriends.js
+++ FrontEnd/src/screens/MyFriends.js
@@ -13,21 +13,24 @@ const MyFriendsScreen = () => {
             <Box mt={6} mb={6} ml={10} mr={2}> 
                 <Grid container columns={12} rowSpacing={6}>
                     {Array.from(Array(numOfFriends)).map((_, index) => {
-                    {/* Sets alignment for FindMoreFriends component. */}
+                        {/* Sets alignment for FindMoreFriends component. */}
                         if (numOfFriends % 2 === 0) {
-                            fmfAlign = "right" }
-                        else {
-                            fmfAlign = "center" }
-                    {/* Sets alignment for friend at index. */}
+                            fmfAlign = "right" 
+                        } else {
+                            fmfAlign = "center" 
+                        }
+                        {/* Sets alignment for friend at index. */}
                         if (index % 2 === 0) {
-                            friendAlign = "right" }
-                        else {
-                            friendAlign = "center" } 
-                    {/* Makes Grid item for Friend at index. */}
+                            friendAlign = "right" 
+                        } else {
+                            friendAlign = "center" 
+                        } 
+                        {/* Makes Grid item for Friend at index. */}
                         return (
                             <Grid item sm={6} key={index} align={friendAlign} style={{ maxWidth: '100%'}}>
                                 <Friend/>
-                            </Grid> )  
+                            </Grid> 
+                        )  
                     })}
                     {/* Makes Grid item for FindMoreFriends. */}
                     <Grid item sm={6} align={fmfAlign} style={{maxWidth: '100%'}}>
