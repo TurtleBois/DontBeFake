69653497b2dedd4bd8c0ae8639083798c602ceb7
nikki-ha <nikkiha811@gmail.com>
2022-11-22 15:30:56 -0800

    Added hrefs.

--- FrontEnd/src/components/FindMoreFriends.js
+++ FrontEnd/src/components/FindMoreFriends.js
@@ -4,9 +4,8 @@ import "../styles/Friend.css"
 const FindMoreFriends = () => {
     return(
         <div class="find-more-friends">
-            {/* Make image link to search page */}
-            <img alt="fmf-img" src={require('../assets/plus-sign.png')}/>
-            <b><p id="find-more-friends-text">Find more friends</p></b>
+            <a class="fmf-link" href="/search"><img alt="fmf-img" src={require('../assets/plus-sign.png')}/></a>
+            <a class="fmf-link" href="/search"><b><p id="find-more-friends-text">Find more friends</p></b></a>
         </div>
     )
 }
--- FrontEnd/src/components/Friends.js
+++ FrontEnd/src/components/Friends.js
@@ -4,11 +4,12 @@ import "../styles/Friend.css"
 const Friend = () => {
     return(
         <body class="friend">
-            <img alt="pfp" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"/>
+            {/* /friendprofile is temporary link */}
+            <a href="/friendprofile" class="friend-link"><img alt="pfp" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"/></a>
             <div class="friend-text">
-                <b><p id="friend-name"></p>Powell Cat</b>
-                <b><p id="friend-username">campuscat</p></b>
-                <b><p id="friend-status">"🐈‍⬛"</p></b>
+                <a href="/friendprofile" class="friend-link"><b><p id="friend-name">Powell Cat</p></b></a>
+                <a href="/friendprofile" class="friend-link"><b><p id="friend-username">campuscat</p></b></a>
+                <a href="/friendprofile" class="friend-link"><b><p id="friend-status">"🐈‍⬛"</p></b></a>
             </div>
         </body>
     )
