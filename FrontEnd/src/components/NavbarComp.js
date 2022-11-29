41bab8e4716a6c7cfcfefb2c479731313ab30793
Jacky Dai <ykcaj@g.ucla.edu>
2022-11-29 01:50:03 -0800

    getting changes for the group display

--- FrontEnd/src/components/NavbarComp.js
+++ FrontEnd/src/components/NavbarComp.js
@@ -19,6 +19,7 @@ import SignUpScreen from '../screens/SignUp';
 import SearchFriendsScreen from '../screens/SearchFriends';
 import Group from '../screens/Group';
 import JoinGroup from '../screens/JoinGroup';
+import viewGroups from '../screens/viewGroups';
 import { useNavigate } from "react-router";
 
 function logout() {
