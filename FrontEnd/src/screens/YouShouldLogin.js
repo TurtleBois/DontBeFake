26f530ed75faf55eea296e8b684dd8cd5d8cc86b
nikki-ha <nikkiha811@gmail.com>
2022-11-28 21:39:36 -0800

    YouShouldLogin screen.

---
+++ FrontEnd/src/screens/YouShouldLogin.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../styles/YouShouldLogin.css"
+
+const YouShouldLogInScreen = () => {
+    return(
+        <b><h4>YouShouldLogin.</h4></b>
+    )
+}
+
+export default YouShouldLogInScreen;
