a796ea88b5a9ad6757e920d1dc7056577b2a2560
Jacky Dai <ykcaj@g.ucla.edu>
2022-11-30 22:15:27 -0800

    changed invies to one col

--- FrontEnd/src/screens/InvitesScreen.js
+++ FrontEnd/src/screens/InvitesScreen.js
@@ -4,14 +4,14 @@ import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 
 const InvitesScreen = () => {
-    var numOfInvites = 6;
+    var numOfInvites = 5;
     return (
         <div>
             <Box pt={3.5} pb={3.5} ml={7} mr={7}> 
                 <Grid container columns={12} rowSpacing={5}>
                     {Array.from(Array(numOfInvites)).map((_, index) => {
                         return (
-                            <Grid item sm={6} key={index} align={"center"} style={{ maxWidth: '100%'}}>
+                            <Grid item sm={12} key={index} align={"center"} style={{ maxWidth: '100%'}}>
                                 <Invite
                                 name="GroupName"
                                 num="#"
