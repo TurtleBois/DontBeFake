6a59cd07cb2f2a8bdb33acd0823e216fff2a856c
Alena <alena.k.zhu@gmail.com>
2022-11-30 17:54:01 -0800

    Group Invites Screen

---
+++ FrontEnd/src/screens/InvitesScreen.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Invite from "../components/Invite";
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+
+const InvitesScreen = () => {
+    var numOfInvites = 6;
+
+    return(
+        <div>
+            <Box pt={3.5} pb={3.5} ml={5} mr={5}>
+                <Grid container columns={12} rowSpacing={5}>
+                    {Array.from(Array(numOfInvites).map((_,index) => {
+                            <Grid item sm={6} key={index} align="center" style={{maxWidth: "100%"}}>
+                                <Invite
+                                    GroupName="GroupName"
+                                    numMembers="#"/>
+                            </Grid>
+                    }))}
+                </Grid>
+            </Box>
+        </div>
+    )
+}
+
+export default InvitesScreen;
