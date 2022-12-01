fb654566fdc9644afb942260bc72ddad21b1095d
nikki-ha <nikkiha811@gmail.com>
2022-11-30 20:39:23 -0800

    Removed link behavior from SearchGroups screen.

--- FrontEnd/src/components/GroupReq.js
+++ FrontEnd/src/components/GroupReq.js
@@ -90,10 +90,10 @@ const GroupReq = (props) => {
         return(
             <body class="user">
                 {/* /friendprofile is temporary link */}
-                <a href="/friendprofile" class="friend-link"><img alt="pfp" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"/></a>
+                <img alt="pfp" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"/>
                 <div class="user-text">
-                    <a href="/friendprofile" class="friend-link"><b><p id="user-name">{props.name} </p></b></a>
-                    <a href="/friendprofile" class="friend-link"><b><p id="user-username">MemberCount: {props.numMembers}</p></b></a>
+                    <b><p id="user-name">{props.name} </p></b>
+                    <b><p id="user-username">MemberCount: {props.numMembers}</p></b>
                 </div>
                 <button id="join-button" ><b>Join.</b></button>
             </body>
@@ -104,10 +104,10 @@ const GroupReq = (props) => {
         return(
             <body class="user">
                 {/* /friendprofile is temporary link */}
-                <a href="/friendprofile" class="friend-link"><img alt="pfp" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"/></a>
+                <img alt="pfp" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"/>
                 <div class="user-text">
-                    <a href="/friendprofile" class="friend-link"><b><p id="user-name">{props.name} </p></b></a>
-                    <a href="/friendprofile" class="friend-link"><b><p id="user-username">MemberCount: {props.numMembers}</p></b></a>
+                    <b><p id="user-name">{props.name} </p></b>
+                    <b><p id="user-username">MemberCount: {props.numMembers}</p></b>
                 </div>
                 <button id="joined-button" disabled><b>Joined.</b></button>
             </body>
