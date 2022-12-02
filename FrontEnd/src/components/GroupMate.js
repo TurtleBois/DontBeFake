8b15e4ca5509fae7c603ddb67c977fa53c8031dc
Jacky Dai <ykcaj@g.ucla.edu>
2022-12-02 06:45:53 -0800

    set up fake voting

--- FrontEnd/src/components/GroupMate.js
+++ FrontEnd/src/components/GroupMate.js
@@ -3,17 +3,48 @@ import "../styles/GroupMate.css"
 
 const GroupMate = (props) => {
     var name = props.name;
+    const [vote, setVote] = React.useState(null);
+
+    const handleYes = () => {
+        
+        console.log("he is fake")
+        var first = false
+        if(vote === null)
+        {
+            console.log("this is your first time voting fake")
+            first = true
+        }
+        setVote(true)
+        props.registerVote(props.userID, true, first)
+        
+      }
+      
+      const handleNo = () => {
+        var first = false
+        if(vote === null)
+        {
+            console.log("this is your first time voting fake")
+            first = true
+        }
+        setVote(false)
+        props.registerVote(props.userID, false, first)
+      }
+
     if(props.name == "") {
         name = "Powell Cat"
     }
+    
+
+
+    // console.log(props)
     return(
         <body class="groupmate">
             <img alt="pfp" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"/>
             <div class="groupmate-text">
                 <b><p id="groupmate-name">{name}</p></b>
                 <b><p id="groupmate-username">@{props.username}</p></b>
-                <button class="voting-buttons" id="real-button"><b>Real.</b></button>
-                <button class="voting-buttons" id="fake-button"><b>Fake.</b></button>
+                <button class={(!vote && vote !== null) ? "pressed-buttons":"voting-buttons" } id="real-button" onClick={handleNo} ><b>Real.</b></button>
+                <button class={(vote && vote !== null) ? "pressed-buttons":"voting-buttons" } id="fake-button" onClick={handleYes}><b>Fake.</b></button>
             </div>
         </body>
     )
